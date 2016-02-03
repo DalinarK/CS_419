77687d6af12de31b8edab3aeaacf9b92d2192aee
Dustin Dinh <djdinh@gmail.com>
2016-02-03 12:03:59 -0800

    successfully read in file to python now need to filter

--- parse.py
+++ parse.py
@@ -4,15 +4,19 @@ import email
 print("Starting python script")
 fileStream = open("pythoncreatedfile", 'w')
 
-fileStream.write('test \n')
+fileStream.write('swag \n')
+for line in sys.stdin:
+	fileStream.write (line),
 
-full_msg = sys.stdin.readlines()
 
-msg = email.message_from_string(full_msg.join());
 
-to = msg['to']
-fromHeader = msg['from']
-subject = msg['subject']
-body = msg['body']
+# full_msg = sys.stdin.readlines()
 
-fileStream.write(subject)
+# msg = email.message_from_string(full_msg.join());
+
+# to = msg['to']
+# fromHeader = msg['from']
+# subject = msg['subject']
+# body = msg['body']
+
+# fileStream.write(subject)
