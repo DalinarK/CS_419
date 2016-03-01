2efe816eba9e4eae393512457372eca4ee5060e0
Erik Ratcliffe <ratclier@oregonstate.edu>
2016-02-29 16:49:20 -0800

    Minor adjustments to comments in testApptList.py.

--- testApptList.py
+++ testApptList.py
@@ -26,16 +26,16 @@ def get_appts():
     padmonth = format(time.strptime(cal_selected['month'], '%B').tm_mon, '02d')
     padday   = format(time.strptime(cal_selected['day'], '%d').tm_mday, '02d')
 
-    # Build an ISO8601 date string
+    # Build an ISO8601 date string (NOTE: Is this really an ISO string?)
     isodate = str(cal_selected['year']) + "-" + str(padmonth) + "-" + str(padday)
 
     # SQLite3 query for appointments that match isodate.
     # Result set includes:
-    #   first_name  (student first name)
-    #   middle_name (student middle name)
-    #   last_name   (student last name)
-    #   appt_id     (appointment ID in the DB)
-    #   start       (ISO date of start time)
+    #   [0] first_name  (student first name)
+    #   [1} middle_name (student middle name)
+    #   [2} last_name   (student last name)
+    #   [3} appt_id     (appointment ID in the DB)
+    #   [4} appt_t      (ISO date of start time)
     sql = "SELECT s.first_name, s.middle_name, s.last_name, a.id AS appt_id, a.date_time_start AS appt_t"
     sql += " FROM appointment AS a JOIN student AS s WHERE s.student_id = a.fk_student_id" 
     sql += " AND appt_t >= date('" + isodate + "') AND appt_t < date('" + isodate + "', '+1 day')"
@@ -58,24 +58,24 @@ def get_appts():
         appt_n = 0
         for row in rows:
 
-            #print row[4]
-
             # Create a datetime object from the appointment time string
             t = time.strptime(row[4], '%Y-%m-%d %H:%M:%S')
             appt_dt = datetime.datetime(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
 
             # Appointment start time: appt_dt.strftime("%-I:%M%p")
 
+            # Build the student name
             s_name = row[0]
             if row[1] != "":
                 s_name += (" " + row[1])
             if row[2] != "":
                 s_name += (" " + row[2])
 
+            # Add the row to the dictionary
             appts[appt_n] = row[3], appt_dt.strftime("%-I:%M%p"), s_name
-            #print appts[appt_n]
             appt_n += 1
 
+    # Print out what we got, just to prove we got something...
     print appts
 
 
